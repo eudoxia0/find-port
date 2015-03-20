f9e68fce9f0daddd63106f63ff152b7d16055adb
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-20 19:05:12 -0300

    Initial commit: system definition and source code

---
+++ find-ports.asd
@@ -0,0 +1,18 @@
+(defsystem find-ports
+  :author "Fernando Borretti <eudoxiahp@gmail.com>"
+  :maintainer "Fernando Borretti <eudoxiahp@gmail.com>"
+  :license "MIT"
+  :version "0.1"
+  :homepage ""
+  :bug-tracker ""
+  :source-control (:git "")
+  :depends-on (:usocket)
+  :components ((:module "src"
+                :serial t
+                :components
+                ((:file "find-ports"))))
+  :description "Find open ports programmatically."
+  :long-description
+  #.(uiop:read-file-string
+     (uiop:subpathname *load-pathname* "README.md"))
+  :in-order-to ((test-op (test-op find-ports-test))))
---
+++ src/find-ports.lisp
@@ -0,0 +1,22 @@
+(in-package :cl-user)
+(defpackage find-ports
+  (:use :cl)
+  (:export :port-open-p
+           :find-port))
+(in-package :find-ports)
+
+(defun port-open-p (port)
+  "Determine if the port is open."
+  (handler-bind
+      ((usocket:address-in-use-error
+         #'(lambda (condition)
+             (ignore condition)
+             nil)))
+    (let ((socket (usocket:socket-listen "127.0.0.1" port
+                                         :reuse-address t)))
+      (usocket:socket-close socket))))
+
+(defun find-port (&key (min 49152) (max 65535))
+  "Return the first available port in a range of port numbers."
+  (loop for port from min to max until (port-open-p port)
+        finally (return port)))
